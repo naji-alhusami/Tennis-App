6f076271e42855e6ca4eaa948ecff15f8b6a878d
naji-alhusami <najihussami@gmail.com>
2024-03-19 15:06:01 +0100

    added traingin part in home page

--- components/Home/Trainings/Trainings.tsx
+++ components/Home/Trainings/Trainings.tsx
@@ -1,17 +1,83 @@
 import Header from "@/components/ui/Header";
 import React from "react";
+import { TrainingLevelsData } from "./TrainingsData";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Image from "next/image";
+import player1 from "@/public/images/player1.jpg";
+import player2 from "@/public/images/player2.jpg";
 
 interface Props {}
 
 const Trainings = () => {
   return (
-    <div className="flex flex-row justify-center items-center my-20">
-      <Header
-        H3Header="Never Too Late"
-        H1Header="TRAINING"
-        H2Header="Certified Coaches"
-        PHeader="Welcome to TENNIS NET club, home to certified coaches dedicated to enhancing your tennis skills. We offer beginner, intermediate, and advanced courses. Join us to elevate your game and enjoy the sport of tennis to the fullest!"
-      />
+    <div className="mx-10 my-40 flex flex-row">
+      <div className="hidden lg:flex lg:flex-row lg:h-full lg:mr-4">
+        <div className="mr-2">
+          <Image
+            className="lg:h-[30rem] xl:h-[29rem]"
+            src={player1}
+            alt="player1"
+          />
+        </div>
+        <div className="ml-2">
+          <Image
+            className="lg:h-[30rem] xl:h-[29rem]"
+            src={player2}
+            alt="player1"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center ">
+        <div>
+          <Header
+            H3Header="Never Too Late"
+            H1Header="TRAINING"
+            H2Header="Certified Coaches"
+            PHeader="Welcome to TENNIS NET club, home to certified coaches dedicated to enhancing your tennis skills. We offer beginner, intermediate, and advanced courses. Join us to elevate your game and enjoy the sport of tennis to the fullest!"
+          />
+        </div>
+        <div className="w-full">
+          {TrainingLevelsData.map((training) => (
+            <div
+              key={training.id}
+              className="w-full h-6 bg-gray-500 relative my-6"
+            >
+              <div
+                className={training.style}
+                // ref={ref}
+                // variants={{
+                //   hidden: { width: 0 },
+                //   visible: { width: `${training.width}%` },
+                // }}
+                // initial="hidden"
+                // animate={mainControls}
+                // transition={{ duration: 0.8, delay: 0.3 }}
+              >
+                <p
+                // ref={ref}
+                // variants={{
+                //   hidden: { opacity: 0, z: 100 },
+                //   visible: { opacity: 1, z: 0 },
+                // }}
+                // initial="hidden"
+                // animate={mainControls}
+                // transition={{ duration: 0.8, delay: 0.3 }}
+                >
+                  {training.courseType}
+                </p>
+                <span className="flex flex-row">
+                  {/* <Number n={training.percent} /> */}
+                  {training.percent}
+                  &#37;
+                </span>
+              </div>
+            </div>
+          ))}
+          <Button className={buttonVariants()}>
+            Check Training Offers &rarr;
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
--- components/Home/Trainings/TrainingsData.tsx
+++ components/Home/Trainings/TrainingsData.tsx
@@ -3,29 +3,29 @@ interface TrainingLevel {
   courseType: string;
   percent: number;
   width: string;
-  style: "greenRectangular" | "yellowFilled" | "greenRectangular2"; // Define allowed styles
+  style: string;
 }
 
 export const TrainingLevelsData: TrainingLevel[] = [
   {
     id: "1",
-    courseType: "BEGINNER COURSES",
+    courseType: "BEGINNER",
     percent: 80,
     width: "80",
-    style: "greenRectangular",
+    style: "font-bold text-white w-4/5 h-6 absolute top-0 bg-yellow-500 flex flex-row items-center justify-between px-5",
   },
   {
     id: "2",
-    courseType: "INTERMEDIATE COURSES",
+    courseType: "INTERMEDIATE",
     percent: 50,
     width: "50",
-    style: "yellowFilled",
+    style: "font-bold text-white w-3/5 h-6 absolute top-0 bg-green-700 flex flex-row items-center justify-between px-5",
   },
   {
     id: "3",
-    courseType: "ADVANCED COURSES",
-    percent: 60,
-    width: "60",
-    style: "greenRectangular2",
+    courseType: "ADVANCED",
+    percent: 50,
+    width: "50",
+    style: "font-bold text-white w-3/5 h-6 absolute top-0 bg-yellow-500 flex flex-row items-center justify-between px-5",
   },
 ];
